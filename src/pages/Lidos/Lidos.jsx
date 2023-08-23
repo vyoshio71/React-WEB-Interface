aaf26483f25cc0321ddbfbefad407d4b170dd6ea
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-08-23 19:30:27 -0300

    feat: add create new components for list unread and read books

--- src/pages/Lidos/Lidos.jsx
+++ src/pages/Lidos/Lidos.jsx
@@ -1,11 +1,33 @@
+import { getJaLiBooks } from "../../Utils/LocalStorageUtils";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import { db } from "../../mocks/itens.mock";
+import "./index.scss";
 
 const Lidos = () => {
+  const jaLiBooksIds = getJaLiBooks();
+  const jaLiBooks = db.filter((book) => jaLiBooksIds.includes(book.id));
   return (
     <div>
       <Header />
-      <body className="body-container"></body>
+      <body className="body-container">
+        <div className="book-content">
+          <h2 className="title">Livros Lidos</h2>
+          {jaLiBooks.map((book) => (
+            <div className="books-box" key={book.id}>
+              <div className="img-style">
+                <img className="book-image" src={book.imageUrl} />
+              </div>
+              <div className="description-details">
+                <h1>{book.title}</h1>
+                <h2>{book.autor}</h2>
+                <h3><b>Genêro:</b>{book.genero}</h3>
+                <p>{book.descricao}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </body>
       <Footer />
     </div>
   );
--- src/pages/ListaDeLeitura/ListaDeLeitura.jsx
+++ src/pages/ListaDeLeitura/ListaDeLeitura.jsx
@@ -1,11 +1,35 @@
+import { getQueroLerBooks } from "../../Utils/LocalStorageUtils";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import { db } from "../../mocks/itens.mock";
 
 const ListaDeLeitura = () => {
+  const queroLerBooksIds = getQueroLerBooks();
+  const queroLerBooks = db.filter((book) => queroLerBooksIds.includes(book.id));
   return (
     <div>
       <Header />
-      <body className="body-container"></body>
+      <body className="body-container">
+        <div>
+          <h2 className="title">Lista de leitura</h2>
+          {queroLerBooks.map((book) => (
+            <div className="books-box" key={book.id}>
+              <div className="img-style">
+                <img className="book-image" src={book.imageUrl} />
+              </div>
+              <div className="description-details">
+                <h1>{book.title}</h1>
+                <h2>{book.autor}</h2>
+                <h3>
+                  <b>Genêro:</b>
+                  {book.genero}
+                </h3>
+                <p>{book.descricao}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </body>
       <Footer />
     </div>
   );
