c8f2531bf68347e300a0dc845b3e4c5802fb3eeb
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-08-23 17:53:06 -0300

    feat: create routes and config pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./global.scss";
+import Home from "./pages/Home/Home";
+import ListaDeLeitura from "./pages/ListaDeLeitura/ListaDeLeitura";
+import Lidos from "./pages/Lidos/Lidos";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/listadeleitura",
+    element: <ListaDeLeitura />,
+  },
+  {
+    path: "/lidos",
+    element: <Lidos />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
