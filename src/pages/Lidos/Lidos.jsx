66a4186e3caaff5900b343b8c771b15dc9f9675b
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-08-23 17:56:09 -0300

    feat: create read component

---
+++ src/pages/Lidos/Lidos.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+const Lidos = () => {
+  return (
+    <div>
+      <Header />
+      <body className="body-container"></body>
+      <Footer />
+    </div>
+  );
+};
+
+export default Lidos;
