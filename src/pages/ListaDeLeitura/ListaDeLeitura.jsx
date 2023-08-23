bd372fbd5eff8114c8b6150fba69b177c3ee7561
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-08-23 17:55:55 -0300

    feat: create list read component

---
+++ src/pages/ListaDeLeitura/ListaDeLeitura.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+const ListaDeLeitura = () => {
+  return (
+    <div>
+      <Header />
+      <body className="body-container"></body>
+      <Footer />
+    </div>
+  );
+};
+
+export default ListaDeLeitura;
