80bf3b75a025b3b495ac56cdab6ed6712dc3b75b
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-08-23 17:55:20 -0300

    feat: create a mock database for books

---
+++ src/mocks/itens.mock.js
@@ -0,0 +1,133 @@
+export const db = [
+  {
+    id: 1,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr7QvOUmL9AHcjETG3o_CptwG8x_U8zkmuZm023bWHydDhQMY9",
+    title: "2001: Uma odisséia no espaço",
+    autor: "Arthur C. Clarke",
+    genero: "Ficção Científica",
+    descricao:
+      "No alvorecer da humanidade, a fome e os predadores já ameaçavam de extinção a incipiente espécie humana. Até que a chegada de um objeto impossível, além da compreensão das mentes limitadas do homem pré-histórico, prenunciasse o caminho da evolução. Milhões de anos depois, a descoberta de um enigmático monolito soterrado na Lua deixa os cientistas perplexos.",
+  },
+
+  {
+    id: 2,
+    imageUrl:
+      "https://m.media-amazon.com/images/I/61bVmoNhVTL._AC_UF1000,1000_QL80_.jpg",
+    title: "A Guerra dos Tronos",
+    autor: "George R. R. Martin",
+    genero: "Fantasia/Drama",
+    descricao:
+      "O verão pode durar décadas. O inverno, toda uma vida. E a guerra dos tronos começou. Como Guardião do Norte, lorde Eddard Stark não fica feliz quando o rei Robert o proclama a nova Mão do Rei. Sua honra o obriga a aceitar o cargo e deixar seu posto em Winterfell para rumar para a corte, onde os homens fazem o que lhes convém, não o que devem... e onde um inimigo morto é algo a ser admirado.",
+  },
+
+  {
+    id: 3,
+    imageUrl:
+      "https://images-americanas.b2w.io/produtos/4177553938/imagens/usado-a-verdade-sobre-o-caso-harry-quebert/4177553938_1_large.jpg",
+    title: "A verdade sobre o caso Harry Quebert",
+    autor: "Joël Dicker",
+    genero: "Ação/Aventura",
+    descricao:
+      "Com apenas vinte e oito anos, publicou um livro que se tornou um best-seller e o alçou ao status de celebridade, com direito a um apartamento chique em Manhattan, um carrão, uma namorada estrela de TV e presenças constantes nos tapetes vermelhos, além de um contrato milionário para um novo romance.",
+  },
+
+  {
+    id: 4,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIyLsR_snShdsSJ9jPsOThdw7oNrsZYDDoxdMdPp0-WdTdN1IP",
+    title: "As Crônicas de Nárnia. Brochura",
+    autor: "C. S. Lewis",
+    genero: "Fantasia",
+    descricao:
+      "Viagens ao fim do mundo, criaturas fantásticas e batalhas épicas entre o bem e o mal - o que mais um leitor poderia querer de um livro? O livro que tem tudo isso é 'O leão, a feiticeira e o guarda-roupa', escrito em 1949 por Clive Staples Lewis.",
+  },
+
+  {
+    id: 5,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGbgOVbzMu8hU4u6-L4BhHPDPRY3rWF8CCNNzfIYzdtQKINHyF2DKpt2TdnAxqaeufAw8&usqp=CAU",
+    title: "Assassinato no Expresso do Oriente",
+    autor: "Agatha Christie",
+    genero: "Ficção",
+    descricao:
+      "Em meio a uma viagem, Hercule Poirot é surpreendido por um telegrama solicitando seu retorno a Londres. Então, o famoso detetive belga embarca no Expresso do Oriente, que está inesperadamente cheio para aquela época do ano.",
+  },
+
+  {
+    id: 6,
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT_7u_MnIDvs73djWZ0UY2G_RV2PWYjSKF1MK5w_8TNNgzf0lrp",
+    title: "Blade Runner",
+    autor: "Philip K. Dick",
+    genero: "Drama",
+    descricao:
+      "Inspiração para um dos maiores clássicos do cinema, dirigido por Ridley Scott, este romance é de autoria do prolífico e revolucionário Philip K. Dick, um dos maiores expoentes da contracultura na ficção científica durante as décadas de 60 e 70. Rick Deckard é um caçador de recompensas, vivendo em uma San Francisco decadente, coberta pela poeira radioativa que dizimou inúmeras espécies de animais e plantas.",
+  },
+
+  {
+    id: 7,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQh5Yqw98q0fZm12d5bvuPQecSXlG5LF8EzE1NeMngjOsQnkDV",
+    title: "Garota exemplar",
+    autor: "Gillian Flynn",
+    genero: "Suspense",
+    descricao:
+      "O livro começa no dia do quinto aniversário de casamento de Nick e Amy Dunne, quando a linda e inteligente esposa de Nick desaparece da casa deles às margens do rio Mississippi. Sinais indicam que se trata de um sequestro violento e Nick rapidamente se torna o principal suspeito.",
+  },
+
+  {
+    id: 8,
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQvjlyEBEixt0rMaqysTgulY-4eglz8E7JqgMLu-9p_FK2Zitd5",
+    title: "Harry Potter e a Pedra Filosofal",
+    autor: "J. K. Rowling",
+    genero: "Fantasia",
+    descricao:
+      "Harry Potter é um garoto cujos pais, feiticeiros, foram assassinados por um poderosíssimo bruxo quando ele ainda era um bebê. Ele foi levado, então, para a casa dos tios que nada tinham a ver com o sobrenatural.",
+  },
+
+  {
+    id: 9,
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSnwDDORPK1BElnmcBWHt7QtnvI98asTZgg4X3Pk8OTPIksu-Eu",
+    title: "Jogador número um",
+    autor: "Ernest Cline",
+    genero: "Fantasia",
+    descricao:
+      "O ano é 2045 e o mundo real é um lugar terrível. Para escapar, a humanidade passa a maior parte do tempo logada no OASIS, uma realidade virtual utópica com milhares de planetas onde as pessoas podem ser o que quiserem e coisas fantásticas acontecem ― magos duelam contra robôs japoneses gigantes, há planetas inteiros inspirados em Blade Runner e DeLoreans voadores podem atingir a velocidade da luz.",
+  },
+
+  {
+    id: 10,
+    imageUrl:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQd-eNi5DjNveKeTcYbluhpcc8qBLoEWgMQKxTEm2vtPQyh5h0q",
+    title: "O morro dos ventos uivantes",
+    autor: "Emily Brontë",
+    genero: "Ficção",
+    descricao:
+      "Único romance da escritora inglesa Emily Bronte, O morro dos ventos uivantes retrata uma trágica historia de amor e obsessão em que os personagens principais são a obstinada e geniosa Catherine Earnshaw e seu irmão adotivo, Heathcliff. Grosseiro, humilhado e rejeitado, ele guarda apenas rancor no coração, mas tem com Catherine um relacionamento marcado por amor e, ao mesmo tempo, ódio.",
+  },
+
+  {
+    id: 11,
+    imageUrl:
+      "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcThlzV_xPxK2wSDRdKRF4Ixkx3CCWJydT40Um-kmRkV56VmvBMk",
+    title: "O grande Gatsby",
+    autor: "F. Scott Fitzgerald",
+    genero: "Graphic Novels",
+    descricao:
+      "O sol em ascensão desse universo cintilante e musical é o enigmático milionário Jay Gatsby, ao redor do qual orbitam três casais glamorosos e desencontrados, numa trama densa, repleta de intrigas, paixões e conflitos que precipitam o trágico eclipse. Recriação soberba de um dos períodos mais prósperos da história dos Estados Unidos, O grande Gatsby é uma crítica mordaz à insensibilidade e imoralidade revestidas de ouro da chamada Era do Jazz, e um dos melhores romances ― talvez o melhor ― já escritos nesse país.",
+  },
+
+  {
+    id: 12,
+    imageUrl:
+      "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQpSMiCYROo7kL04EO1c9SuT7kG8UOgInOU21AnHhlUY9Kv87dx",
+    title: "Orgulho e preconceito",
+    autor: "Jane Austen",
+    genero: "Ficção",
+    descricao:
+      "Austen nos apresenta Elizabeth Bennet como heroína irresistível e seu pretendente aristocrático, o sr. Darcy. Nesse livro, aspectos diferentes são abordados: orgulho encontra preconceito, ascendência social confronta desprezo social, equívocos e julgamentos antecipados conduzem alguns personagens ao sofrimento e ao escândalo.",
+  },
+];
